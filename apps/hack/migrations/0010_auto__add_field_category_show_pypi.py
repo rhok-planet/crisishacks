b0c6ac7415de15fa5f95728d3cfebb1599305f82
Josh Livni <jlivni@google.com>
2010-10-08 15:06:16 -0700

    typo in schema migration

--- apps/hack/migrations/0010_auto__add_field_category_show_pypi.py
+++ apps/hack/migrations/0010_auto__add_field_category_show_pypi.py
@@ -7,11 +7,11 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
+       pass 
 
 
     def backwards(self, orm):
-        
+       pass 
 
 
     models = {
